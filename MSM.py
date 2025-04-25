69a54d9c6daa2c924adc8bee2e200b8a34e736c5
Horita <ryohorita105@gmail.com>
2025-04-25 09:51:43 +0900

    Update MSM.py

--- MSM.py
+++ MSM.py
@@ -200,7 +200,7 @@ ds['lon'].attrs['units'] = 'degrees_east'
 # 0度以下におけるMSMとGSMの飽和水蒸気量の定義の違いを補正
 mask = ds['temperature'] <= 273.15
 temp = ds['temperature'] - 273.15
-ds['relative_humidity'] = xr.where(mask, ds['relative_humidity'] * (10 ** (7.5 * temp / (237.3 + temp) - 9.5 * temp /(265.5 + temp))))
+ds['relative_humidity'] = xr.where(mask, ds['relative_humidity'] * (10 ** (7.5 * temp / (237.3 + temp) - 9.5 * temp /(265.5 + temp))), ds['relative_humidity'])
 
 ## 必要な物理量を計算する
 ds['dewpoint_temperature'] = mpcalc.dewpoint_from_relative_humidity(ds['temperature'], ds['relative_humidity'] * 1000 * units('g/kg'))
