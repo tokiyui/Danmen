324910b08b54dcdf9a4e12552657f5c047f2f0d8
Horita <ryohorita105@gmail.com>
2025-05-15 15:44:52 +0900

    Update MFR.py

--- MFR.py
+++ MFR.py
@@ -297,4 +297,4 @@ for date in dates:
     ax.axvline(date, linestyle='--', linewidth=1)
 
 fig.text(0.5,0.01,"{:.2f}N, {:.2f}E GFS FT{:03d}-{:03d} {}Z Initial".format(lats[i_lat],lons[i_lon],fts[0],fts[-1],dt), ha='center',va='bottom', size=15)
-plt.savefig('gfs.png')
+plt.savefig('mfr.png')
