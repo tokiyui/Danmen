76de4fb705ce564ef82042cf2ec8c5f24b9b5373
Horita <ryohorita105@gmail.com>
2026-06-11 08:28:26 +0900

    Update MSM.py

--- MSM.py
+++ MSM.py
@@ -131,7 +131,7 @@ for i in range(t_size): # ft の時間ループ
     file_path = os.path.join(dat_fld, gr_fn)
     if not os.path.exists(gr_fn):
         print(f"ダウンロード開始: {gr_fn}")
-        response = requests.get(dat_fld + gr_fn)
+        response = requests.get(dat_fld + gr_fn, verify=False)
         response.raise_for_status()  # エラーがあれば例外を発生
 
     # ダウンロードしたコンテンツをローカルに保存
